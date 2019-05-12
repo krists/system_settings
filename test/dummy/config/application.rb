ff209f8f227db7b0e38a6619e1ec0a5b023e9ce4
Krists Ozols <krists.ozols@upb.lv>
2019-05-12 16:55:24 +0300

    Fix dummy test app CSRF

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -20,5 +20,8 @@ module Dummy
       config.ssl_options = { hsts: { subdomains: true } }
     end
     config.api_only = true
+    config.session_store :cookie_store, key: "system_settings_sid_#{Rails.env}"
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
   end
 end
