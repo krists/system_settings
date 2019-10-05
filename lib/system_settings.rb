8f76368a45d619d21499802fdd103fc058034e5b
Krists Ozols <krists.ozols@upb.lv>
2019-10-05 17:56:14 +0300

    Make missing setting error message frozen-string friendly

--- lib/system_settings.rb
+++ lib/system_settings.rb
@@ -9,11 +9,14 @@ module SystemSettings
     instrument("system_settings.find", name: name) do |payload|
       record = Setting.find_by(name: name)
       unless record
-        message = "Couldn't find system setting #{name}\n\n"
-        message << "It might not be loaded from settings file(#{settings_file_path}).\n"
-        message << "To load missing settings with their initial values you "
-        message << "can call SystemSettings.load from your Rails environment or run Rails task:\n\n"
-        message << "    bin/rails system_settings:load RAILS_ENV=#{::Rails.env}"
+        message = <<~MESSAGE.strip
+          Couldn't find system setting #{name}
+
+          It might not be loaded from settings file(#{settings_file_path}).
+          To load missing settings with their initial values you can call SystemSettings.load from your Rails environment or run Rails task:
+
+              bin/rails system_settings:load RAILS_ENV=#{::Rails.env}
+        MESSAGE
         raise(Errors::NotFoundError, message)
       end
       payload[:value] = record.value
