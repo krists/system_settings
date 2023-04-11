6aefd67a04e6ca5306b538f6bfa70d53042a8fea
Krists Ozols <krists.ozols@gmail.com>
2023-04-11 16:37:27 +0300

    Bounce favicon.ico

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount SystemSettings::Engine => "/system_settings"
   get "action_button", to: "action_buttons#index"
+  get "favicon.ico" => lambda { |_| [204, {}, []] }
   root to: redirect("/system_settings")
 end
